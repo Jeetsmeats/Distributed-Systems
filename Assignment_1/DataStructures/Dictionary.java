6c54c68343082a7809bfe772ab0810e11bfe00df
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-09-01 01:47:41 +1000

    Successfully serialised the Dictionary data structure.
    Removed WordDescription.java, reduced complexity of code.

--- Assignment_1/DataStructures/Dictionary.java
+++ Assignment_1/DataStructures/Dictionary.java
@@ -5,20 +5,21 @@ import java.util.HashMap;
 import Exceptions.*;
 import Helper.*;
 import java.io.*;
+import java.util.*;
 
 /***
  * Word Dictionary Data Structure
  */
-public class Dictionary {
+public class Dictionary implements Serializable {
 
     /**
      * Word Dictionary Hashmap
      */
-    private HashMap<String, WordDescription> WordDictionary;
+    private HashMap<String, ArrayList<String>> WordDictionary;
 
     public Dictionary() {
 
-        this.WordDictionary = new HashMap<String, WordDescription>();
+        this.WordDictionary = new HashMap<String, ArrayList<String>>();
 
     }
 
@@ -28,26 +29,14 @@ public class Dictionary {
         // create sample data
         try {
 
-            WordDescription wd1 = new WordDescription("Novel", "A long, fictional narrative that describes intimate human experiences, typically in prose form.");
-            wd1.addDescription("New and not resembling something formerly known or used.");
-
-            WordDescription wd2 = new WordDescription("Bank", "The land alongside or sloping down to a river or lake.");
-            wd2.addDescription("An establishment for the custody, loan, exchange, or issue of money, for the extension of credit, and for facilitating the transmission of funds.");
-
-            WordDescription wd3 = new WordDescription("Aloof", "Not friendly or forthcoming; cool and distant.");
-
-            WordDescription wd4 = new WordDescription("Ephemeral", "Lasting for a very short time");
-
-            WordDescription wd5 = new WordDescription("Serendipity", "The occurrence and development of events by chance in a happy or beneficial way.");
-
-            WordDescription wd6 = new WordDescription("Obfuscate", "To render obscure, unclear, or unintelligible");
-
-            tempDictionary.put(wd1.getWord(), wd1);
-            tempDictionary.put(wd2.getWord(), wd2);
-            tempDictionary.put(wd3.getWord(), wd3);
-            tempDictionary.put(wd4.getWord(), wd4);
-            tempDictionary.put(wd5.getWord(), wd5);
-            tempDictionary.put(wd6.getWord(), wd6);
+            tempDictionary.addWord("Novel", "A long, fictional narrative that describes intimate human experiences, typically in prose form.");
+            tempDictionary.addDescription("New and not resembling something formerly known or used.", "Novel");
+            tempDictionary.addWord("Bank", "The land alongside or sloping down to a river or lake.");
+            tempDictionary.addDescription("An establishment for the custody, loan, exchange, or issue of money, for the extension of credit, and for facilitating the transmission of funds.", "Bank");
+            tempDictionary.addWord("Aloof", "Not friendly or forthcoming; cool and distant.");
+            tempDictionary.addWord("Ephemeral", "Lasting for a very short time");
+            tempDictionary.addWord("Serendipity", "The occurrence and development of events by chance in a happy or beneficial way.");
+            tempDictionary.addWord("Obfuscate", "To render obscure, unclear, or unintelligible");
 
         } catch (InvalidDescription e) {
 
@@ -65,20 +54,14 @@ public class Dictionary {
         }
     }
 
-    /**
-     * Put method for word description data
-     * @param wd word description data
-     */
-    public void put(String word, WordDescription wd) {
-        this.WordDictionary.put(word, wd);
-    }
+
     /**
      * Get the list of word meanings.
      * @param word Selected word
      * @throws WordNullException word in dictionary error
-     * @return Get the list of word description.
+     * @return Get the list of word descriptions.
      */
-    public WordDescription getMeaning(String word) throws WordNullException {
+    public ArrayList<String> getMeaning(String word) throws WordNullException {
 
         if (this.WordDictionary.containsKey(word)) {
 
@@ -100,7 +83,8 @@ public class Dictionary {
         // check word validity
         try {
 
-            checkWord(word);
+            checkWordExists(word);
+            checkWordIsValid(word);
         } catch (WordNullException e) {
 
             e.printStackTrace();
@@ -128,17 +112,106 @@ public class Dictionary {
         // check word validity
         try {
 
-            checkWord(word);
-        } catch (WordNullException e) {
-
-            e.printStackTrace();
+            checkWordIsValid(word);
         } catch (InvalidWordException e) {
 
             e.printStackTrace();
         }
 
+        ArrayList<String> descriptions = new ArrayList<String>();
+        descriptions.add(description);
 
-        this.WordDictionary.put(word, new WordDescription(word, description));
+        this.WordDictionary.put(word, descriptions);
+    }
+
+    /**
+     * @return list of word descriptions.
+     */
+    public ArrayList<String> getWordDescription(String word) {
+
+        return this.WordDictionary.get(word);
+    }
+
+    /**
+     * Add description to word in dictionary
+     * @param description word description
+     * @throws InvalidDescription entered description already exists
+     */
+    public void addDescription(String description, String word) throws InvalidDescription {
+
+        try {
+
+            // check description validity
+            checkDescription(description);
+        } catch (InvalidDescription e) {
+
+            e.printStackTrace();
+        }
+
+        // check if the description already exists
+        if (checkDescriptionExists(description, word) >= 0) throw new InvalidDescription("Description already exists, give a new description.");
+        // add description
+        this.WordDictionary.get(word).add(description);
+    }
+
+    /**
+     * Update the word description
+     * @param newDescription updated description for word.
+     * @throws DescriptionNullException
+     */
+    public void updateDescription(String newDescription, String word) throws DescriptionNullException {
+
+        // check the description
+        // description validity check
+        try {
+
+            checkDescription(newDescription);
+
+            // get the description index in array
+            int descIdx = checkDescriptionExists(newDescription, word);
+
+            if (descIdx == -1) throw new DescriptionNullException("Description does not exist.");
+
+            // update description
+            this.WordDictionary.get(word).set(descIdx, newDescription);
+        } catch (InvalidDescription e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check if the description is valid.
+     * @param description description to check validity.
+     * @throws Exceptions.InvalidDescription
+     */
+    private void checkDescription(String description) throws InvalidDescription {
+
+        CheckData.CheckDataHelper checkDataHelper = new CheckData.CheckDataHelper();
+        if (checkDataHelper.onlyDigits(description)) {          /* Description is only numbers exception */
+
+            throw new InvalidDescription("Description cannot be only numbers.");
+        } else if (description.length() > 1000) {
+
+            throw new InvalidDescription("The description is too long.  Keep the description less than 1000 characters.");
+        }
+    }
+
+    /**
+     * Check if description exist for the word.
+     * @param description description to check
+     * @return description pointer.
+     */
+    private int checkDescriptionExists(String description, String word) {
+
+        int i = 0;
+        for (String desc: this.WordDictionary.get(word)) {
+
+            if (desc.equals(description)) return i;
+            i++;
+        }
+
+        return -1;
     }
 
     /**
@@ -147,27 +220,25 @@ public class Dictionary {
      * @throws InvalidWordException
      * @throws WordNullException
      */
-    private void checkWord(String word) throws InvalidWordException, WordNullException {
+    private void checkWordIsValid(String word) throws InvalidWordException {
 
         CheckData.CheckDataHelper checkDataHelper = new CheckData.CheckDataHelper();
         if (checkDataHelper.onlyDigits(word)) {           /* Check if the word has only digits*/
 
             throw new InvalidWordException("Word cannot be only numbers.");
-        } else if (!this.WordDictionary.containsKey(word)) {            /* Check if the word exists in the dictionary */
+        }
+    }
+
+    /**
+     * Check if the word exists.
+     * @param word word to check the existence of.
+     * @throws WordNullException Word does not exist.
+     */
+    private void checkWordExists(String word) throws WordNullException {
+
+        if (!this.WordDictionary.containsKey(word)) {            /* Check if the word exists in the dictionary */
 
             throw new WordNullException("Word is not in the dictionary.");
         }
     }
-
-//    private void readData() {
-//
-//        try (FileInputStream fileIn = new FileInputStream("Dictionary.ser");
-//             ObjectInputStream in = new ObjectInputStream(fileIn)) {
-//
-//            this.WordDictionary = (HashMap<String, WordDescription>) in.readObject();
-//        } catch (IOException | ClassNotFoundException i) {
-//
-//            i.printStackTrace();
-//        }
-//    }
 }
